a0da286ab0acd2fe0a933665b4b68051aac667fa
CosmeMendesRocha <44973015+CosmeMendesRocha@users.noreply.github.com>
2018-11-20 16:20:48 -0300

    Paginas navegaveis

--- visivel.js
+++ visivel.js
@@ -1,1 +1,48 @@
-
+function Home(){
+    document.querySelector('.Home').style.display='inline';
+    document.querySelector('.login_cadastro').style.display='none';
+    document.querySelector('.empresas').style.display='none';
+    document.querySelector('.pontos_de_coleta').style.display='none';
+    document.querySelector('.mapa').style.display='none';
+    document.querySelector('.parques_eco').style.display='none';
+}
+function Login_Cadastro(){
+    document.querySelector('.Home').style.display='none';
+    document.querySelector('.login_cadastro').style.display='inline';
+    document.querySelector('.empresas').style.display='none';
+    document.querySelector('.pontos_de_coleta').style.display='none';
+    document.querySelector('.mapa').style.display='none';
+    document.querySelector('.parques_eco').style.display='none';
+}
+function Empresas(){
+    document.querySelector('.Home').style.display='none';
+    document.querySelector('.login_cadastro').style.display='none';
+    document.querySelector('.empresas').style.display='inline';
+    document.querySelector('.pontos_de_coleta').style.display='none';
+    document.querySelector('.mapa').style.display='none';
+    document.querySelector('.parques_eco').style.display='none';
+}
+function Pontos_de_coleta(){
+    document.querySelector('.Home').style.display='none';
+    document.querySelector('.login_cadastro').style.display='none';
+    document.querySelector('.empresas').style.display='none';
+    document.querySelector('.pontos_de_coleta').style.display='inline';
+    document.querySelector('.mapa').style.display='none';
+    document.querySelector('.parques_eco').style.display='none';
+}
+function Mapa(){
+    document.querySelector('.Home').style.display='none';
+    document.querySelector('.login_cadastro').style.display='none';
+    document.querySelector('.empresas').style.display='none';
+    document.querySelector('.pontos_de_coleta').style.display='none';
+    document.querySelector('.mapa').style.display='inline';
+    document.querySelector('.parques_eco').style.display='none';
+}
+function Parques_Eco(){
+    document.querySelector('.Home').style.display='none';
+    document.querySelector('.login_cadastro').style.display='none';
+    document.querySelector('.empresas').style.display='none';
+    document.querySelector('.pontos_de_coleta').style.display='none';
+    document.querySelector('.mapa').style.display='none';
+    document.querySelector('.parques_eco').style.display='inline';
+}
